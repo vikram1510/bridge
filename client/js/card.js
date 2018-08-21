9b77cab276de98c58b66523d6f633de4dd569787
aerolith07 <aerolith07@gmail.com>
2018-08-21 21:31:37 +0100

    Added Card object

---
+++ client/js/card.js
@@ -0,0 +1,32 @@
+function card(suit,number) {
+  // Card Render
+
+  // temp for rect
+  this.xPos = 10;
+  this.yPos = 10;
+  this.width = 30;
+  this.height = 60;
+
+  this.suit = suit;
+  this.number = number;
+
+  // Set Color
+  if (suit == "spadesuit" | "clubsuit" | "blackjoker") {
+    this.color = "black";
+  } else if (suit == "heartsuit" | "diamondsuit" | "redjoker") {
+    this.color = "red"
+  } else {
+    this.color = "notset" // Deal with setting colours for joker
+  }
+
+  this.played = false;
+}
+
+card.prototype.moveCard = function() {
+  // console.log("Moved Card");
+  this.xPos = this.xPos + 10;
+
+  if (this.xPos > 500) {
+    this.xPos = 10;
+  }
+}
