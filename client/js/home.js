df877a11ca91c4d8866e3073b1b5d7817020cc7a
vikram1510 <42555296+vikram1510@users.noreply.github.com>
2018-08-26 22:34:51 +0100

    set state funcy

--- client/js/home.js
+++ client/js/home.js
@@ -1,29 +1,11 @@
-//TODO List
-//
-//
-//
-//
-//
-//
-
-
-var states = {
-    wait: false,
-    middleSelect: false,
-    trumpSelect: false,
-    gameMode: false,
-    finishGame: false,
+var stateList = {
+    'wait': false,
+    'middleSelect': false,
+    'trumpSelect': false,
+    'gameMode': false,
+    'finishGame': false,
 }
 var waitMsg='';
-
-
-var wait = {
-    status:  false,
-    message: ''
-}
-var middle
-
-
 
 var ready = false;
 var socket = io.connect();
@@ -80,160 +62,142 @@ function preload() {
 
 function setup() {
 
-teamnameDropdown.onchange = function() {
-  var teamname = teamnameDropdown.value
-}
-
-playernameField.oninput = function(e) {
-  e.preventDefault();
-  var name = playernameField.value
-  console.log(name);
-};
-
-setState = function(state){
-
-
-
-
-}
-
-
-setupDeck = function(inputcards) {
-
-  if(windowWidth > 900 && windowHeight > 300)   {
-    cardWidth = windowWidth/11;
-    cardHeight =  cardWidth * 1.4;
-  }
-
-  deckStartX = (windowWidth - ((cardWidth-30)*inputcards.length))/2;
-  deckStartY = windowHeight-cardHeight+30;
-
-  var d = 0;
-  for(var i in inputcards) {
-    // inputcards[i].overlap = (30-d)*i;
-    inputcards[i].xPos = deckStartX+((cardWidth-30-d)*i);
-    if(inputcards[i].chosen == false) {
-    inputcards[i].yPos = deckStartY;
-    } else {
-    inputcards[i].yPos = deckStartY-60;
+    teamnameDropdown.onchange = function() {
+      var teamname = teamnameDropdown.value
     }
-    d++;
-  }
 
-  deckWidth = inputcards[inputcards.length-1].xPos - inputcards[0].xPos + cardWidth;
+    playernameField.oninput = function(e) {
+      e.preventDefault();
+      var name = playernameField.value
+      console.log(name);
+    };
 
-}
+    setState = function(givenState){
+        for (var state in stateList){
+            if(state==givenState){
+                stateList[state]= true;
+            }
+            else{stateList[state]= false;}
+        }
+    }
 
-playButtonClicked = function(){
-  var teamname = teamnameDropdown.value;
-  var name = playernameField.value;
+    setupDeck = function(inputcards) {
 
-  var data = {
-        name : name,
-        teamname : teamname
+      if(windowWidth > 900 && windowHeight > 300)   {
+        cardWidth = windowWidth/11;
+        cardHeight =  cardWidth * 1.4;
       }
 
+      deckStartX = (windowWidth - ((cardWidth-30)*inputcards.length))/2;
+      deckStartY = windowHeight-cardHeight+30;
 
-  socket.emit('signInRequest', data);
-
-  console.log("Waiting for server response");
-
-  socket.on('signInResponse', function(data) {
-    if (data.connected) {
-        signInPage.style.display = 'none';
-        // gamePage.style.display = '';
-        createCanvas(windowWidth,windowHeight);
-
-      } else {
-        console.log("Can not connect");
+      var d = 0;
+      for(var i in inputcards) {
+        // inputcards[i].overlap = (30-d)*i;
+        inputcards[i].xPos = deckStartX+((cardWidth-30-d)*i);
+        if(inputcards[i].chosen == false) {
+        inputcards[i].yPos = deckStartY;
+        } else {
+        inputcards[i].yPos = deckStartY-60;
+        }
+        d++;
       }
-  })
 
-  socket.on('readyToPlay', function(data) {
-        inputcards = data.myCards;
-        setupDeck(inputcards);
-        ready = true;
-  })
+      deckWidth = inputcards[inputcards.length-1].xPos - inputcards[0].xPos + cardWidth;
 
-}
+    }
+
+    playButtonClicked = function(){
+      var teamname = teamnameDropdown.value;
+      var name = playernameField.value;
+
+      var data = {
+            name : name,
+            teamname : teamname
+          }
 
 
-signInForm.onsubmit = function(e) {
-    e.preventDefault();
-    playButtonClicked();
-}
+      socket.emit('signInRequest', data);
 
-playbtn.onclick = playButtonClicked;
+      console.log("Waiting for server response");
+
+      socket.on('signInResponse', function(data) {
+        if (data.connected) {
+            signInPage.style.display = 'none';
+            // gamePage.style.display = '';
+            createCanvas(windowWidth,windowHeight);
+
+          } else {
+            console.log("Can not connect");
+          }
+      })
+
+      socket.on('readyToPlay', function(data) {
+            inputcards = data.myCards;
+            setupDeck(inputcards);
+            ready = true;
+      })
+
+    }
+
+
+    signInForm.onsubmit = function(e) {
+        e.preventDefault();
+        playButtonClicked();
+    }
+
+    playbtn.onclick = playButtonClicked;
 
 }
 
 
 function draw() {
 
-if (ready == true) {
-background(24, 141, 74)
+    if (ready) {
+    background(24, 141, 74)
 
-var c = 12;
-for(var i in inputcards) {
+    var c = inputcards.length-1;
+    for(var i in inputcards) {
 
-    // MOUSEOVER Detection
-    if(mouseY > inputcards[c].yPos &&  mouseX > inputcards[c].xPos &&
-       mouseX < inputcards[c].xPos + cardWidth) { // Mouse is between input card and no other cards selected
+        // MOUSEOVER Detection
+        if(mouseY > inputcards[c].yPos &&  mouseX > inputcards[c].xPos &&
+           mouseX < inputcards[c].xPos + cardWidth) { // Mouse is between input card and no other cards selected
 
-        inputcards[c].selected = true;
+            inputcards[c].selected = true;
 
-        if(inputcards[c].yPos > deckStartY-60 && inputcards[c].selected) { // Bring up card position
-          inputcards[c].yPos = inputcards[c].yPos - 10;
+            if(inputcards[c].yPos > deckStartY-60 && inputcards[c].selected) { // Bring up card position
+              inputcards[c].yPos = inputcards[c].yPos - 10;
+            }
+        } else {
+          inputcards[c].selected = false;
         }
-    } else {
-      inputcards[c].selected = false;
+
+
+        if (inputcards[c].selected == false && inputcards[c].yPos < deckStartY) {
+            inputcards[c].yPos = inputcards[c].yPos + 10;
+        }
+
+      c--;
+
+      if(inputcards[i].chosen == true) {
+          // tint(0, 153, 204);
+          inputcards[i].yPos = deckStartY-70;
+        }
+      var img = image(cardimgarray[inputcards[i].suit+inputcards[i].number], inputcards[i].xPos , inputcards[i].yPos, cardWidth , cardHeight);
+
     }
 
+    playcardbtnWidth = 120;
+    playcardbtnHeight = 30;
+    fill(20,200,30);
+    noStroke();
+    rect(deckStartX + deckWidth/2 -playcardbtnWidth/2, deckStartY-110, playcardbtnWidth ,playcardbtnHeight,5);
 
-    if (inputcards[c].selected == false && inputcards[c].yPos < deckStartY) {
-        inputcards[c].yPos = inputcards[c].yPos + 10;
+    var textstring = mouseX + "," + mouseY;
+    fill(255);
+    text(textstring,50,50);
+
     }
-
-  c--;
-
-  // OLD LOGIC FOR DISPLAYING CARDS
-  // fill(255);
-  // stroke(200);
-  // strokeWeight(1);
-  // var bool = inputcards[i].selected == true || inputcards[i].chosen == true;
-  // console.log(bool);
-  // if(inputcards[i].selected == true || inputcards[i].chosen == true) {
-  //   fill(240);
-  //   stroke(2);
-  //   strokeWeight(2);
-  // }
-  // rect(inputcards[i].xPos , inputcards[i].yPos, cardWidth , cardHeight , 10);
-  // fill(inputcards[i].color);
-  // textSize(20);
-  // textStyle(BOLD);
-  // noStroke();
-  // text(inputcards[i].number, inputcards[i].xPos + 15, inputcards[i].yPos + 25);
-
-  if(inputcards[i].chosen == true) {
-      // tint(0, 153, 204);
-      inputcards[i].yPos = deckStartY-70;
-    }
-  var img = image(cardimgarray[inputcards[i].suit+inputcards[i].number], inputcards[i].xPos , inputcards[i].yPos, cardWidth , cardHeight);
-
-
-}
-
-playcardbtnWidth = 120;
-playcardbtnHeight = 30;
-fill(20,200,30);
-noStroke();
-rect(deckStartX + deckWidth/2 -playcardbtnWidth/2, deckStartY-110, playcardbtnWidth ,playcardbtnHeight,5);
-
-var textstring = mouseX + "," + mouseY;
-fill(255);
-text(textstring,50,50);
-
-}
 
 }
 
